0a6b62093da61b0b665e52cc0e96ed3c4296e8e2
KlemerMonteiro <klemermonteiro93@gmail.com>
2019-05-14 11:24:28 -0300

    Comittando o primeiro simbolo

---
+++ models/simbol.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export default class Simbol extends Project{
+    
+
+}
+module.exports = (sequelize, Datatypes) => {
+        var Simbol = sequelize.define('Simbol', {
+            
+            simbolId: DataTypes.STRING;
+            nomeSimbol: Datatypes.STRING;
+            sinonimosSimbol []: DataTypes.STRING;
+            nocaoSimbol: DataTypes.STRING;
+            impactoSimbol: DataTypes.STRING;
+            classificaSimbol: DataTypes.STRING
+        });
+
+    Simbol.associate = function(models){
+        models.Simbol.BelongsTo(models.project);
+        models.Simbolo.hasMany(models.sinonimosSimbol);
+    };
+
+        return Simbol;
+};
