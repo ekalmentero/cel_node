3912c7349fc799ace4cb7059801f5dbe47dff37b
laripaiva <lariiiipaiva@gmail.com>
2019-05-30 21:20:16 -0300

    alterações aí

--- controller/cenario.controller.js
+++ controller/cenario.controller.js
@@ -1,14 +1,75 @@
-var model = require('../models/index');
+var model = require("../models/index");
 
 exports.viewAll = (req, res) => {
-  model.Cenario.findAll({ include: [{ all: true, nested: true }]})
-  	.then(cenarios => res.json({
-  		error: false,
-  		data: cenarios
-  	}))
-  	.catch(error => res.json({
-  		error: true,
-  		data: [],
-  		error: error
-  	}));
+  model.Cenario.findAll({ include: [{ all: true, nested: true }] })
+    .then(cenarios =>
+      res.json({
+        error: false,
+        data: cenarios
+      })
+    )
+    .catch(error =>
+      res.json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
+};
+
+exports.updateAtor = (req, res) => {
+  const id = req.params.id;
+  const idator = req.params.idator;
+  model.Ator.update(
+    { nome: req.body.nome },
+    {
+      where: {
+        id: req.params.idator,
+        cenarioId: req.params.id
+      }
+    }
+  ).then(() => {
+    res.status(200).send("updated.");
+  });
+};
+
+exports.updateContexto = (req, res) => {
+  const idcenario = req.params.id;
+  const idcontexto = req.params.idcontexto;
+  model.Contexto.update(
+    { Descricao: req.body.descricao },
+    {
+      where: {
+        id: req.params.idcontexto,
+        cenarioId: req.params.id
+      }
+    }
+  ).then(() => {
+    res.status(200).send("updated.");
+  });
+};
+
+exports.updateEpisodio = (req, res) => {
+  const idcenario = req.params.id;
+  const idepisodio = req.params.idepisodio;
+  model.Episodio.update(
+    { descricao: req.body.descricao, tipo: req.body.tipo },
+    {
+      where: {
+        id: req.params.idepisodio,
+        cenarioId: req.params.id
+      }
+    }
+  ).then(() => {
+    res.status(200).send("updated.");
+  });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  model.Cenario.destroy({
+    where: { id: id }
+  }).then(() => {
+    res.status(200).send("Cenário com o id=" + id + " deletado com sucesso");
+  });
 };
--- routes/cenario.js
+++ routes/cenario.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var cenarioController = require('../controller/cenario.controller');
-
+var cenarioController = require("../controller/cenario.controller");
 
 /* GET cenário listing. */
-router.get('/', cenarioController.viewAll);
-
+router.get("/", cenarioController.viewAll);
+router.put("/:id/atores/:idator", cenarioController.updateAtor);
+router.put("/:id/contexto/:idcontexto", cenarioController.updateContexto);
+router.put("/:id/episodio/:idepisodio", cenarioController.updateEpisodio);
+router.delete("/:id", cenarioController.delete);
 module.exports = router;
